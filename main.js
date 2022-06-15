be3feb40a194b676df812662831ac3a2549d5b8f
Yasomalako <Yasomalako@gmail.com>
2022-06-15 13:20:40 +0300

    train swich case

---
+++ main.js
@@ -0,0 +1,41 @@
+//1
+// function prinRigtnum(){
+// let number = +prompt('add anuber');
+// switch (number) {
+//     case 3:
+//         console.log('hello');
+//         return;
+//     case 5:
+//         console.log('good bay')
+//         return;
+//     case 7:
+//         console.log('tanke you')
+//     return;
+//     default:
+//         console.log('ur nuber is rong')
+//         return;
+// }
+// }
+// prinRigtnum()
+//1//
+// function cheakNames(){
+//     let userName = prompt('enter your name pleas')
+//     switch(userName){
+//         case 'jacob':
+//             document.write(userName)
+//             return;
+//         case 'nathan':
+//             document.write(userName.toUpperCase())
+//             return;
+//         case 'DALYA':
+//             document.write(userName.toLowerCase())
+//             return;
+//             default:
+//                 document.write('wrong name')
+
+       
+//     }
+// }
+// cheakNames()
+//3/
+function printOperator
